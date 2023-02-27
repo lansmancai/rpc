997eb0428587e6319209eea6087570ce9ae9126a
jinniu <649312549@qq.com>
2023-02-27 17:17:33 +0800

    rpcv0

---
+++ rpcv0/src/main/java/com/lansmancai/rpcv0/service/UserService.java
@@ -0,0 +1,9 @@
+package com.lansmancai.rpcv0.service;
+
+
+import com.lansmancai.rpcv0.common.User;
+
+public interface UserService {
+    // 客户端通过这个接口调用服务端的实现类
+    User getUserByUserId(Integer id);
+}
